60f3d8d8f4c15e814b1b12e46493c6fa1ac0bdfc
Lucas Burlingham <lucas@lucasburlingham.me>
2023-02-12 12:15:47 -0500

    Upload to sync

--- new/index.php
+++ new/index.php
@@ -1,4 +1,29 @@
 <?php
 
-echo "hi!";
+if(isset($_POST['site'])) {
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+
+	$db = new SQLite3('db\users.db');
+
+
+	$db->exec("INSERT INTO users (name, email) VALUES ('$name', '$email')");
+	
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=One">
+	<title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
